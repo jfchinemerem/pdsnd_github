ac20f7c9aaca4258e3ca8abd4f3776372f9a932b
Chinemerem-Jonathan <chinemerem03@gmail.com>
2023-11-14 23:49:50 +0100

    Hints user to correct cities to add

--- bikeshare.py
+++ bikeshare.py
@@ -17,7 +17,7 @@ def get_filters():
         if city in CITY_DATA:
             break
         else:
-            print("Invalid city name. Please enter a valid city name.")
+            print("Invalid city name. Please enter a valid city name: chicago, new_york_city or washington.")
 
     while True:
         month = input("Name of the month to filter by, or 'all' to apply no month filter: ").lower()
